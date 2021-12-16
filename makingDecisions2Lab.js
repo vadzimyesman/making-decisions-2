0f7f0938386855a4502642104a65ac4114b812ae
Vadzim_Yesman <vvyesman@gmail.com>
2021-12-16 12:52:43 -0800

    problems 1-9

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -122,9 +122,15 @@ console.log(someNum)
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3.
+// If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
+for (i=0; i<listOfNumbers.length; i++){
+  if (listOfNumbers[i]%3==0){
+    console.log(`${listOfNumbers[i]} is divisible by 3`)
+  }
+}
 
 
 ////////// PROBLEM 9 //////////
@@ -137,7 +143,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (i=letters.length-1;i>=0;i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
